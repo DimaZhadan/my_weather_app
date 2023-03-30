a606a84f274d43abc749baaa0bb83cf127e3ede6
Dima Zhadan <dimka.jadanxaxa@gmail.com>
2023-03-30 19:19:38 +0300

    add styles and change weather vindow

--- src/components/minWeatheer/MineWeather.js
+++ src/components/minWeatheer/MineWeather.js
@@ -13,6 +13,7 @@ const MineWeather = () => {
    const [loading, setLoading] = useState(true);
    const [renderWEather, setRenderWEather] = useState(false);
    const [count, setCount] = useState(8);
+   const [error, setError] = useState(false);
 
    const { getCitiCoordinates, getWeatherInfo } = useApiServices();
 
@@ -26,7 +27,8 @@ const MineWeather = () => {
       getWeatherInfo(item.lat, item.lon, count)
          .then(weather => setWeat(weather))
          .then(onWeatherLoading)
-         .then(onRenderWeather);
+         .then(onRenderWeather)
+         .catch(onError);
    }
 
    const onWeatherLoading = () => {
@@ -38,10 +40,10 @@ const MineWeather = () => {
    }
 
    const onCountChange = (num) => {
-      setRenderWEather(false)
-      setLoading(true)
-      setCount(num)
-      onWeatherCountChange(num, weather)
+      setRenderWEather(false);
+      setLoading(true);
+      setCount(num);
+      onWeatherCountChange(num, weather);
    }
 
 
@@ -50,7 +52,11 @@ const MineWeather = () => {
       setRenderWEather(true)
       getWeatherInfo(weather.city.coord.lat, weather.city.coord.lon, count)
          .then(weather => setWeat(weather))
+         .catch(onError)
+   }
 
+   const onError = () => {
+      setError(true)
    }
 
    function renderItem(arr) {
@@ -80,42 +86,44 @@ const MineWeather = () => {
    const loader = loading ? <Spinner /> : null;
    const content = renderWEather ? <WeateherComp data={weather} /> : null;
    const defCity = cityName ? <span className='weather__city-name'> {cityName}</span> : <span className='weather__city-def'> Оберіть місто</span>;
+   const errorMassege = error ? <div>Щось пішло нре так...</div> : null;
 
    return (
       <div className='weather'>
          <div className="container">
             <div className="weather__grid">
-            <div className="data">
-               <div className='weather__city'>Погодні умови у місті: <label htmlFor='input-city'> {defCity}</label></div>
-               <div className='weather__input'>
-                  <input
-                     onInput={(e) => onCitySelect(e.target.value)}
-                     placeholder="Введіть назву міста/селища"
-                     className='weather__input-field'
-                     id='input-city' />
-               </div>
-               <div className='weather__city-wrapper'>
-                  {cities}
-               </div>
-               <div className="weather__count">
-                  Оберіть період:
-                  <div className='weather__count-wrapper'>
-                     <button
-                        onClick={() => onCountChange(16)}
-                        className='weather__count-btn'>Дні: 2</button>
-                     <button
-                        onClick={() => onCountChange(24)}
-                        className='weather__count-btn'>Дні: 3</button>
-                     <button
-                        onClick={() => onCountChange(40)}
-                        className='weather__count-btn'>Днів: 5</button>
+               <div className="data">
+                  <div className='weather__city'>Погодні умови у місті: <label htmlFor='input-city'> {defCity}</label></div>
+                  <div className='weather__input'>
+                     <input
+                        onInput={(e) => onCitySelect(e.target.value)}
+                        placeholder="Введіть назву міста/селища"
+                        className='weather__input-field'
+                        id='input-city' />
+                  </div>
+                  <div className='weather__city-wrapper'>
+                     {cities}
+                     {errorMassege}
+                  </div>
+                  <div className="weather__count">
+                     Оберіть період:
+                     <div className='weather__count-wrapper'>
+                        <button
+                           onClick={() => onCountChange(16)}
+                           className='weather__count-btn'>Дні: 2</button>
+                        <button
+                           onClick={() => onCountChange(24)}
+                           className='weather__count-btn'>Дні: 3</button>
+                        <button
+                           onClick={() => onCountChange(40)}
+                           className='weather__count-btn'>Днів: 5</button>
+                     </div>
                   </div>
                </div>
-            </div>
-            <div className="content">
-               {loader}
-               {content}
-            </div>
+               <div className="content">
+                  {loader}
+                  {content}
+               </div>
             </div>
          </div>
       </div>
--- src/components/weaterComp/WeatherComp.js
+++ src/components/weaterComp/WeatherComp.js
@@ -1,26 +1,38 @@
-
+import './weatherComp.scss';
 
 const WeateherComp = (props) => {
 
    const weateherData = props.data.list;
 
+   const toNoramlDate = (date) => {
+      const presDay = date;
+      const predDay = presDay.substring(8, 10);
+      const predMonth = presDay.substring(5, 7);
+      const predTime = presDay.substring(10, 16)
+      return (
+         <div>{predDay}.{predMonth} {predTime}</div>
+      )
+   }
+
    function renderItem(weateherData) {
       const items = weateherData.map((item, i) => {
          return (
-            <div key={i}>
-               <div>{item.dt_txt}</div>
+            <div key={i} className="weater-items__item">
+               <div>{toNoramlDate(item.dt_txt)}</div>
+               <div>
+                  <img src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`} alt="weateher icon" />
+               </div>
+               <div>{item.weather[0].description}</div>
                <div>Температура: {Math.round(item.main.temp)} °C</div>
                <div>відчувається як: {Math.round(item.main.feels_like)} °C</div>
                <div>Вологість: {item.main.humidity} %</div>
                <div>Тиск: {item.main.pressure} гПк</div>
-               <div>{item.weather[0].description}</div>
-               <img src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`} alt="" />
             </div>
          )
       })
 
       return (
-         <div className='fff'>
+         <div className='weater-items'>
             {items}
          </div>
       )
